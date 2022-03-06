59f29a31ccba90e3916ec5584af4806aca657859
MilanCu <phuong.dong.cu@gmail.com>
2022-03-06 10:13:51 +0100

    add Config

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const Welcome = () => {
+
+    const [message, setMessage] = useState('Loaduju...');
+
+    const fetchMessage = () =>{
+        axios.get("http://localhost:8080/").then(res =>{
+            setMessage(res.data);
+        })
+    }
+
+    useEffect(()=>{
+        fetchMessage()
+    },[])
+
+    return (
+        <div>
+            {message}
+        </div>
+    )
+}
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
+        <div>
+            <Welcome/>
         </div>
     );
 }
--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/ReservationSystemApplication.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/ReservationSystemApplication.java
@@ -2,9 +2,17 @@ package cz.cvut.fel.rsp.ReservationSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ReservationSystemApplication {
+
+	@GetMapping("/")
+	public String welcome(){
+		return "welcome";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationSystemApplication.class, args);
