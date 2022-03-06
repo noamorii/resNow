3c69bed06f9c1b42f7a14589363c9acc929d1dd4
premekb <38076249+premekb@users.noreply.github.com>
2022-03-06 13:44:01 +0100

    Repository configuration test

---
+++ src/test/java/cz/cvut/fel/rsp/ReservationSystem/dao/GeneralRepositoryTest.java
@@ -0,0 +1,32 @@
+package cz.cvut.fel.rsp.ReservationSystem.dao;
+
+import cz.cvut.fel.rsp.ReservationSystem.model.reservation.slots.Seat;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.EntityManager;
+
+@DataJpaTest
+public class GeneralRepositoryTest {
+    @Autowired
+    private EntityManager em;
+
+    @Autowired
+    private SeatRepository seatRepository;
+
+    @Test
+    public void persist_persistOneEntity_entityPersisted() {
+        Seat seat = new Seat();
+        seat.setSeatIdentifier("x");
+        seat.setPrice(100);
+
+        seatRepository.save(seat);
+        final Seat result = em.find(Seat.class, seat.getId());
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(100, result.getPrice());
+        Assertions.assertEquals("x", result.getSeatIdentifier());
+    }
+}
