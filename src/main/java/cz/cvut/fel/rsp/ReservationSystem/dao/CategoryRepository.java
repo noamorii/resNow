edd62806d706dfd0bb8b69cc70801dc93eff4ff4
premekb <38076249+premekb@users.noreply.github.com>
2022-03-15 00:15:53 +0100

    Category, feedback repositories

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/dao/CategoryRepository.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/dao/CategoryRepository.java
@@ -2,6 +2,8 @@ package cz.cvut.fel.rsp.ReservationSystem.dao;
 
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/dao/FeedbackRepository.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/dao/FeedbackRepository.java
@@ -2,6 +2,8 @@ package cz.cvut.fel.rsp.ReservationSystem.dao;
 
 import cz.cvut.fel.rsp.ReservationSystem.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
 }
