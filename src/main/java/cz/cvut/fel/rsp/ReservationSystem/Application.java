24a487a7a8afa887471df65f85a1e073c9f2cdf6
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-03-19 18:51:44 +0100

    test endpoint in Heroku

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/Application.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/Application.java
@@ -3,9 +3,17 @@ package cz.cvut.fel.rsp.ReservationSystem;
 import cz.cvut.fel.rsp.ReservationSystem.model.user.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Welcome";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
