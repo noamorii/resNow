7184bbf1bd263ebe34741fd79ed743ac004b0196
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-03-19 23:16:22 +0100

    update baseURL

--- src/frontend/src/pages/AboutUs.js
+++ src/frontend/src/pages/AboutUs.js
@@ -5,7 +5,7 @@ const AboutUs = () => {
     return (
         <div className={'presentation-container'}>
             <iframe
-                src="https://docs.google.com/presentation/d/e/2PACX-1vRrcRGj9YI9psmWZ4alkZ47gHoq6-nRE47RHO7LpgxLUcfADH-kmPtdyxI0diCmvR0S-9jDH_jK1xCP/embed?start=false&loop=true&delayms=3000"
+                src="https://docs.google.com/presentation/d/e/2PACX-1vRrcRGj9YI9psmWZ4alkZ47gHoq6-nRE47RHO7LpgxLUcfADH-kmPtdyxI0diCmvR0S-9jDH_jK1xCP/embed?start=false&loop=true&delayms=15000"
                 width="1280" height="749" allowFullScreen="true" mozallowfullscreen="true"
                 webkitallowfullscreen="true"/>
         </div>
