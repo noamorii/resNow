667941c5c05cdcdb14d82171ed6504f38b63fa57
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-03-21 00:30:04 +0100

    init

--- src/frontend/src/config/const.js
+++ src/frontend/src/config/const.js
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://rynary.herokuapp.com'
+export const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080' : 'https://reservenow.herokuapp.com'
