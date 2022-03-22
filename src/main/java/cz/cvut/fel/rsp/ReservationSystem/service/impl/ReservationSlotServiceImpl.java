791631c40cc5a183452a3cbe7766342c4baf4223
premekb <38076249+premekb@users.noreply.github.com>
2022-03-22 02:54:56 +0100

    Generation of seats begin - unfinished

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/service/impl/ReservationSlotServiceImpl.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/service/impl/ReservationSlotServiceImpl.java
@@ -1,19 +1,30 @@
 package cz.cvut.fel.rsp.ReservationSystem.service.impl;
 
+import cz.cvut.fel.rsp.ReservationSystem.dao.SeatRepository;
+import cz.cvut.fel.rsp.ReservationSystem.model.enums.Repetition;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.events.CustomTimeEvent;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.events.Event;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.events.IntervalEvent;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.events.SeatEvent;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.slots.Interval;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.slots.ReservationSlot;
+import cz.cvut.fel.rsp.ReservationSystem.model.reservation.slots.Seat;
 import cz.cvut.fel.rsp.ReservationSystem.service.interfaces.ReservationSlotService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
 
 @Service
+@Transactional
+@RequiredArgsConstructor
 public class ReservationSlotServiceImpl implements ReservationSlotService {
+
+    private final SeatRepository seatRepository;
+
     @Override
     public void generateTimeSlots(Event event) {
         event.visit(this);
@@ -26,12 +37,34 @@ public class ReservationSlotServiceImpl implements ReservationSlotService {
 
     @Override
     public void generateSeatSlots(SeatEvent event) {
+        int seatAmount = event.getSeatAmount();
+        Repetition repetition = event.getRepetition();
+        LocalDate currentDate = event.getStartDate();
+        LocalDate endDate = event.getRepeatUntil();
 
+        while (repetition.equals(Repetition.NONE) || currentDate.isAfter(endDate)){
+            for (int i = 0; i < seatAmount; i++){
+                Seat seat = new Seat();
+                seat.setPrice(100); // TODO hardcoded price
+                seat.setDate(currentDate);
+                seat.setSeatIdentifier(String.valueOf(i));
+                seat.setEvent(event);
+                seatRepository.save(seat);
+            }
+
+            if (repetition.equals(Repetition.NONE)) break;
+
+            currentDate = incrementByRepetition(currentDate, repetition);
+        }
     }
 
     @Override
     public void generateCustomTimeSlots(CustomTimeEvent event) {
 
+    }
+
+    private LocalDate incrementByRepetition(LocalDate date, Repetition repetition){
+        return null;
     }
 
     @Override
