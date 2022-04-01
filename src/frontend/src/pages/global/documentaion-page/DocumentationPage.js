670d7ce57ebae35f500b6e4dbed981344d51c54f
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-04-01 18:33:00 +0200

    add css rule

--- src/frontend/src/pages/global/documentaion-page/DocumentationPage.js
+++ src/frontend/src/pages/global/documentaion-page/DocumentationPage.js
@@ -1,9 +1,93 @@
 import styles from './DocumentationPage.module.scss'
 
-export const DocumentationPage = ()=>{
-    return(
-        <div>
+import {useState} from "react";
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import {docco} from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-        </div>
-    )
+export const DocumentationPage = () => {
+
+    return (
+        <div className={styles.contents}>
+            <div>
+                <h2>Buttons</h2>
+                <h3>primary</h3>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<button className={'button-primary sm'}>Foo</button>`}
+                    </SyntaxHighlighter>
+                    <button className={"button-primary sm"} type={'button'}>Foo</button>
+                </div>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<button className={'button-primary lg'}>Foo</button>`}
+                    </SyntaxHighlighter>
+                    <button className={"button-primary lg"} type={'button'}>Foo</button>
+                </div>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<button className={'button-primary'}>Foo</button>`}
+                    </SyntaxHighlighter>
+                    <button className={"button-primary"} type={'button'}>Foo</button>
+                </div>
+
+
+
+                <h3>outline</h3>
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<button className={'button-primary-outline'}>Foo</button>`}
+                    </SyntaxHighlighter>
+                    <button className={"button-primary-outline"} type={'button'}>Foo</button>
+                </div>
+                <h3>drop-shadow</h3>
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<button className={'button-primary bx-sh'}>Foo</button>`}
+                    </SyntaxHighlighter>
+                    <button className={'button-primary bx-sh'} type={'button'}>Foo</button>
+                </div>
+                <h2>Inputs</h2>
+                <h3>primary</h3>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<input className={'input-primary sm'} placeholder={'Foo'}/>`}
+                    </SyntaxHighlighter>
+                    <input className={'input-primary sm'} placeholder={'Foo'}/>
+                </div>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<input className={'input-primary lg'} placeholder={'Foo'}/>`}
+                    </SyntaxHighlighter>
+                    <input className={'input-primary lg'} placeholder={'Foo'}/>
+                </div>
+
+
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<input className={'input-primary'} placeholder={'Foo'}/>`}
+                    </SyntaxHighlighter>
+                    <input className={'input-primary'} placeholder={'Foo'}/>
+                </div>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<input className={'input-primary correct'} placeholder={'Foo'}/>`}
+                    </SyntaxHighlighter>
+                    <input className={'input-primary correct'} placeholder={'Foo'}/>
+                </div>
+
+                <div className={styles.content}>
+                    <SyntaxHighlighter language="javascript" style={docco}>
+                        {`<input className={'input-primary error'} placeholder={'Foo'}/>`}
+                    </SyntaxHighlighter>
+                    <input className={'input-primary error'} placeholder={'Foo'}/>
+                </div>
+            </div>
+        </div>)
 }
