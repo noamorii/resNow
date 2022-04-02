dcffe621b9f373eff72043eb87281b9f386a0505
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-04-02 12:21:57 +0200

    add DocumentationPage.js

--- src/frontend/src/components/box-green/GreenBox.js
+++
@@ -1,9 +0,0 @@
-import classes from './GreenBox.module.scss';
-
-export const GreenBox = () => {
-    return (
-        <div className={classes.box}>
-            <div className={classes.text}>Green box</div>
-        </div>
-    )
-}
--- src/frontend/src/components/box-red/RedBox.js
+++
@@ -1,9 +0,0 @@
-import classes from './RedBox.module.css';
-
-export const RedBox = () => {
-    return (
-        <div className={classes.box}>
-            Red box
-        </div>
-    )
-}
