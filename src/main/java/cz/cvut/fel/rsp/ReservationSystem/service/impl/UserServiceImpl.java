a3f99ad88bdf5eb36faceddf908007b396c7e058
norbaart <norbaart@fel.cvut.cz>
2022-04-04 23:11:13 +0200

    start user service

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/service/impl/UserServiceImpl.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/service/impl/UserServiceImpl.java
@@ -1,13 +1,29 @@
 package cz.cvut.fel.rsp.ReservationSystem.service.impl;
 
+import cz.cvut.fel.rsp.ReservationSystem.dao.PaymentDetailsRepository;
+import cz.cvut.fel.rsp.ReservationSystem.dao.UserRepository;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.Reservation;
 import cz.cvut.fel.rsp.ReservationSystem.model.user.PaymentDetails;
 import cz.cvut.fel.rsp.ReservationSystem.model.user.User;
 import cz.cvut.fel.rsp.ReservationSystem.service.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    private final PaymentDetailsRepository paymentDetailsRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(PaymentDetailsRepository paymentDetailsRepository, UserRepository userRepository) {
+        this.paymentDetailsRepository = paymentDetailsRepository;
+        this.userRepository = userRepository;
+    }
+
+
     @Override
     public void addPaymentDetails(User user, PaymentDetails paymentDetails) {
 
