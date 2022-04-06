08328949538db8a6ee34c6bee768055fe2c8e5a3
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-04-06 17:01:19 +0200

    delete endpoint

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/Application.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/Application.java
@@ -7,13 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class Application {
-
-	@GetMapping("/welcome")
-	public String welcome(){
-		return "Welcome";
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
