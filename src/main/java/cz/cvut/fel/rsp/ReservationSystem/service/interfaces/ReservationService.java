8d860e1ab66622d99dfd0f9557eb694c41ae2ea6
Jan Pivoňka <pivonja1@fel.cvut.cz>
2022-04-12 11:05:33 +0200

    rebase

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/service/interfaces/ReservationService.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/service/interfaces/ReservationService.java
@@ -1,17 +1,16 @@
 package cz.cvut.fel.rsp.ReservationSystem.service.interfaces;
 
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.Reservation;
-import cz.cvut.fel.rsp.ReservationSystem.model.reservation.ReservationSystem;
-import cz.cvut.fel.rsp.ReservationSystem.model.reservation.Source;
 import cz.cvut.fel.rsp.ReservationSystem.model.reservation.slots.ReservationSlot;
 import cz.cvut.fel.rsp.ReservationSystem.model.user.User;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
-import java.time.LocalTime;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public interface ReservationService{
+
+
+
     public void createReservation(User user, ReservationSlot reservationSlot);
 
     public void cancelReservation(Reservation reservation);
