aa9a42c4c49e054cd97814d1171365669398f6c6
Mukan Atazhanov <atazhanovmukan@gmail.com>
2022-04-14 15:10:00 +0200

    beta-omega-alpha version of clientTermin TODO getters and event persist.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AboutUs from "./pages/global/welcome-page/AboutUs";
 import Welcome from "./pages/global/welcome-page/Welcome";
 import {DocumentationPage, IndexPage, Test} from "./pages";
+import Termin from "./pages/client/client-termin/clientTermin";
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
                 <Route exact  path="/about" element={<AboutUs/>}/>
                 <Route exact  path="/fe-template-documentation" element={<DocumentationPage/>}/>
                 <Route exact  path="/milancu-test" element={<Test/>}/>
+                <Route exact path="/client/termin" element={<Termin/>}/>
             </Routes>
         </BrowserRouter>
     )
