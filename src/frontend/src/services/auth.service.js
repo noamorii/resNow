94d87caed13881f96d2083ed89fc0ba6cf73288c
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-04-15 15:17:55 +0200

    resolve conflict

--- src/frontend/src/services/auth.service.js
+++ src/frontend/src/services/auth.service.js
@@ -3,25 +3,30 @@ import {baseUrl} from "../config/const";
 
 const login = (username, password) => {
     return (axios.post(`${baseUrl}/signin`, {}, {
-            params: {"username": username, "password": password}
-        }).then(response => {
-            if (response.data.accessToken) {
-                localStorage.setItem('user', JSON.stringify(response.data));
-            }
-            return response.data
-        }));
+        params: {"username": username, "password": password}
+    }).then(response => {
+        if (response.data.accessToken) {
+            localStorage.setItem('user', JSON.stringify(response.data));
+        }
+        return response.data
+    }));
 }
 
 const logout = () => {
     localStorage.removeItem('user');
 }
 
-const register = (firstname, lastname, username, email, password) => {
+const register = (firstname, lastname, username, email, password, userType) => {
     return (axios.post(`${baseUrl}/signup`, {}, {
-            params: {
-                "firstname": firstname, "lastname": lastname, "username": username, "email": email, "password": password
-            }
-        }));
+        params: {
+            "firstname": firstname,
+            "lastname": lastname,
+            "username": username,
+            "email": email,
+            "password": password,
+            "userType": userType
+        }
+    }));
 }
 
 const getCurrentUser = () => {
