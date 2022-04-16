37bc8d1cf67518a3f069fcd8c8af82755a9879cd
Phuong Dong Cu <cuphuon3@fel.cvut.cz>
2022-04-16 13:32:12 +0000

    Resolve "vytvořit stránku | client/resources/places"

--- src/frontend/src/pages/client/resources-page/sources/Sources.js
+++ src/frontend/src/pages/client/resources-page/sources/Sources.js
@@ -1,101 +1,143 @@
-import styles from './Sources.module.scss'
+import styles from './Sources.module.scss';
+import {useState, useEffect, useMemo} from "react";
+import {useTable, useFilters, usePagination} from "react-table";
+import MOCK_DATA from "./MOCK_DATA.json"
 
 const Table = ({columns, data}) => {
-    // const {
-    //     getTableProps,
-    //     getTableBodyProps,
-    //     headerGroups,
-    //     rows,
-    //     prepareRow,
-    // } = useTable({
-    //     columns,
-    //     data,
-    // })
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        state,
+        prepareRow,
+    } = useTable({
+        columns,
+        data,
+    },
+        useFilters,
+        usePagination
+    )
+
+    const {pageIndex} = state
 
     return (
-        <table>
+        <>
+        <table {...getTableProps()}>
             <thead>
-            <tr>
-                <th>
-                    <input type="checkbox"/>
-                </th>
-                <td>
-                    <p>Plný název</p>
-                </td>
-                <td>
-                    <p>Název</p>
-                </td>
-                <td>
-                    <p>Služba</p>
-                </td>
-                <td>
-                    <p>Místo</p>
-                </td>
-                <td>
-                    <p>Zaměstnanec</p>
-                </td>
-                <td>
-                    <p></p>
-                </td>
-            </tr>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        <td className={styles.collCheckbox}>
+                            <input type="checkbox" />
+                        </td>
+                        {headerGroup.headers.map(column => (
+                            <td {...column.getHeaderProps()}>
+                                <p>{column.render('Header')}</p>
+                                <div>{column.canFilter ? column.render('Filter') : null}</div>
+                            </td>
+
+                        ))}
+                    </tr>
+                ))}
             </thead>
-            <tbody>
-            <tr>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td>
-                    <button>Upravit</button>
-                    <button>Odstranit</button>
-                </td>
-            </tr>
+            <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+                prepareRow(row)
+                return (
+                    <tr className={styles.tRow} {...row.getRowProps()}>
+                        <td className={styles.collCheckbox}>
+                            <input type="checkbox" />
+                        </td>
+                        {row.cells.map(cell => {
+                            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                        })}
+                        <td>
+                            <div className={styles.buttonCell}>
+                                <button className={'button-primary-outline ' .concat(styles.buttonEdit)}>Upravit</button>
+                                <button className={'button-primary-outline ' .concat(styles.buttonDelete)}>Odstranit</button>
+                            </div>
+                        </td>
+                    </tr>
+                )
+            })}
             </tbody>
         </table>
+        <div className={styles.buttonsContainer}>
+            <div>
+                <span>Page {pageIndex + 1} of {pageOptions.length} </span>
+                <button onClick={() => previousPage()} disabled={!canPreviousPage} className={'button-primary sm'}>Předchozí</button>
+                <button onClick={() => nextPage()} disabled={!canNextPage} className={'button-primary sm'}>Další</button>
+            </div>
+        </div>
+        </>
+    )
+}
+
+const Filter = ({column}) => {
+    const {filterValue, setFilter} = column
+
+    return (
+        <span>
+            <input value={filterValue} onChange={(e) => setFilter(e.target.value)} className={'input-primary search sh sm'} placeholder={'Hledaný text…'}/>
+        </span>
     )
 }
 
 export const Sources = () => {
 
     // const [data, setData] = useState();
-    // const columns = useMemo(() => [
-    //     {
-    //         Header: "Zdroje",
-    //         columns: [
-    //             {
-    //                 Header: "Plný název"
-    //             },
-    //             {
-    //                 Header: "Název"
-    //             },
-    //             {
-    //                 Header: "Služba",
-    //                 Cell: ({ cell: { value } }) => value ? {value} : "-"
-    //             },
-    //             {
-    //                 Header: "Místo"
-    //             },
-    //             {
-    //                 Header: "Zaměstnanec",
-    //                 Cell: ({ cell: { value } }) => value ? {value} : "-"
-    //             },
-    //             {
-    //                 Header: " "
-    //             },
-    //         ]
-    //
-    //     }]
-    // )
+    const data = useMemo(() => MOCK_DATA, [])
+    const columns = useMemo(() => [
+
+                {
+                    Header: "Plný název",
+                    accessor:"full_name",
+                    Filter: Filter
+                },
+                {
+                    Header: "Název",
+                    accessor:"title",
+                    Filter: Filter
+                },
+                {
+                    Header: "Služba",
+                    accessor:"service",
+                    Filter: Filter
+                },
+                {
+                    Header: "Místo",
+                    accessor:"place",
+                    Filter: Filter
+                },
+                {
+                    Header: "Zaměstnanec",
+                    accessor:"employee",
+                    Filter: Filter
+                },
+            ],
+        []
+    )
 
     return (
-        <div>
+        <div className={styles.body}>
             <div className={styles.buttonContainer}>
                 <button className={'button-primary '.concat(styles.button)}>Nový zdroj</button>
             </div>
-            <div className={styles.table}><Table/></div>
+            <div className={styles.table}>
+                <Table columns={columns} data={data}/>
+            </div>
+            <div className={styles.selectContainer}>
+                <select className={styles.select}>
+                    <option selected>-</option>
+                    <option value="Remove">Odstranit</option>
+                </select>
+                <button className={'button-primary sm '}>Aplikovat</button>
+            </div>
         </div>
     )
 }
