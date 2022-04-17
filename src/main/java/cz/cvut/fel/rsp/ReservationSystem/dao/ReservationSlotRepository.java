ba83afe580e3de5d9811935b800eb2dcb96c2ebc
premekb <38076249+premekb@users.noreply.github.com>
2022-04-17 15:34:18 +0200

    fix tests

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/dao/ReservationSlotRepository.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/dao/ReservationSlotRepository.java
@@ -22,16 +22,16 @@ public interface ReservationSlotRepository extends JpaRepository<ReservationSlot
                     "WHERE rs.event.id = ?1)")
     public List<ReservationSlot> findFreeByEventId(Integer eventId);
 
-    @Query(value = "SELECT r FROM ReservationSlot r WHERE r.event.id = :event_id AND r.date >= :from AND r.date <= :to")
-    public List<ReservationSlot> findByEventId(@Param("event_id") Integer eventId, @Param("from") LocalDate from, @Param("to") LocalDate to);
+    @Query(value = "SELECT r FROM ReservationSlot r WHERE r.event.id = :event_id AND r.date >= :from_date AND r.date <= :to_date")
+    public List<ReservationSlot> findByEventId(@Param("event_id") Integer eventId, @Param("from_date") LocalDate from, @Param("to_date") LocalDate to);
 
     @Query(value = "SELECT rs FROM ReservationSlot rs JOIN Reservation re ON (re.reservationSlot.id = rs.id AND re.cancelled = false) " +
-            "WHERE rs.event.id = :event_id AND rs.date >= :from AND rs.date <= :to")
-    public List<ReservationSlot> findReservedByEventId(@Param("event_id") Integer eventId, @Param("from") LocalDate from, @Param("to") LocalDate to);
+            "WHERE rs.event.id = :event_id AND rs.date >= :from_date AND rs.date <= :to_date")
+    public List<ReservationSlot> findReservedByEventId(@Param("event_id") Integer eventId, @Param("from_date") LocalDate from, @Param("to_date") LocalDate to);
 
-    @Query(value = "SELECT rs FROM ReservationSlot rs WHERE rs.event.id = ?1 AND rs NOT IN " +
+    @Query(value = "SELECT rs FROM ReservationSlot rs WHERE rs.event.id = :event_id AND rs NOT IN " +
             "(SELECT rs FROM ReservationSlot rs JOIN Reservation re ON (re.reservationSlot.id = rs.id AND re.cancelled = false) " +
             "WHERE rs.event.id = :event_id)" +
-            "AND rs.date >= :from AND rs.date <= :to")
-    public List<ReservationSlot> findFreeByEventId(@Param("event_id") Integer eventId, @Param("from") LocalDate from, @Param("to") LocalDate to);
+            "AND rs.date >= :from_date AND rs.date <= :to_date")
+    public List<ReservationSlot> findFreeByEventId(@Param("event_id") Integer eventId, @Param("from_date") LocalDate from, @Param("to_date") LocalDate to);
 }
