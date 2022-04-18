0d43f1ca82f358bcfc71dfb227dec60002107fff
Aiya Rakhimova <rakhiaiy@fel.cvut.cz>
2022-04-18 18:43:49 +0200

    customer profile page

--- src/frontend/src/pages/customer/profile-page/ProfilePage.customer.js
+++ src/frontend/src/pages/customer/profile-page/ProfilePage.customer.js
@@ -3,7 +3,48 @@ import styles from './ProfilePage.module.scss'
 export const ProfilePageCustomer = () => {
     return (
         <div>
-            ProfilePage
+            <div className={styles.mainText}>MY PROFILE</div>
+
+            <div className={styles.section}>
+                <p>Credentials</p>
+                <div className={styles.block}>
+                    <div className={styles.fieldsGroup}>
+                        <div>
+                            <p>Name</p>
+                            <input className={'input-primary '.concat(styles.input)} placeholder={'Name'}/>
+                        </div>
+
+                        <div>
+                            <p>Surname</p>
+                            <input className={'input-primary '.concat(styles.input)} placeholder={'Surname'}/>
+                        </div>
+                    </div>
+
+                    <button className={'button-primary'}>Save</button>
+                    <button className={'button-primary-outline'}>Cancel</button>
+                </div>
+            </div>
+
+            <div className={styles.section}>
+                <p>Security</p>
+                <div className={styles.block}>
+                    <div className={styles.fieldsGroup}>
+                        <div>
+                            <p>Current password</p>
+                            <input className={'input-primary '.concat(styles.input)} placeholder={'Password'}/>
+                        </div>
+
+                        <div>
+                            <p>New password</p>
+                            <input className={'input-primary '.concat(styles.input)} placeholder={'New password'}/>
+                        </div>
+                    </div>
+
+                    <button className={'button-primary'}>Save</button>
+                    <button className={'button-primary-outline'}>Cancel</button>
+                </div>
+            </div>
         </div>
+
     )
 }
