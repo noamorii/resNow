5dda2a89a30d9e38af6681219ba6bd68c35223c5
najmanmat <matous@najman.cz>
2022-04-19 10:24:08 +0200

    first table

--- src/frontend/src/pages/client/reservations-page/ReservationsPage.client.js
+++ src/frontend/src/pages/client/reservations-page/ReservationsPage.client.js
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import {baseUrl} from "../../../config/const";
 
+
 const TopButtons = () => {
 
     return (
@@ -22,12 +23,12 @@ const DatePicker = () => {
 
     const [value, setValue] = useState(new DateObject())
 
-    const [data, setData] = useState()
+    const [data1, setData] = useState()
 
     const getData = (e) => {
         axios.get(`${baseUrl}/systems/1/`).then(result => setData(result.data.name))
         setValue(e)
-        console.log(data)
+        console.log(data1)
     }
 
     return (
@@ -62,12 +63,86 @@ const DatePicker = () => {
     )
 }
 
+const TableUnder = () => {
+
+    const [confirm, setConfirm] = useState(false)
+
+    return (
+        <div>
+            <div className={styles.tableContainer}>
+                <table>
+                    <thead>
+                    <tr>
+                        <td className={styles.collCheckbox}>
+                            <input type="checkbox" />
+                        </td>
+                        <td className={styles.collMid}>
+                            <p>EVENT</p>
+                        </td>
+                        <td className={styles.collWide}>
+                            <p>RESERVATION CODE</p>
+                            <input className={'input-primary search sh sm'} placeholder={'Find names'}/>
+                        </td>
+                        <td className={styles.collMid}>
+                            <p>PRICE</p>
+                        </td>
+                        <td className={styles.collNarrow}>
+                            <p>CAP.</p>
+                        </td>
+                        <td className={styles.collWide}>
+                            <p>CUSTOMER</p>
+                            <input className={'input-primary search sh sm'} placeholder={'Find names'}/>
+                        </td>
+                        <td className={styles.collMid}>
+                            <p>STATE</p>
+                        </td>
+                        <td className={styles.collWide}>
+                            <p>DATE OF CANCELLATION</p>
+                        </td>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <th className={styles.collCheckbox}>
+                            <input className={'checkbox'} type="checkbox" />
+                        </th>
+                        <th>event time</th>
+                        <th>reservation code</th>
+                        <th>price</th>
+                        <th>1</th>
+                        <th>customer</th>
+                        <th>state</th>
+                        <th>date of cancellation</th>
+                        <th>
+                            <div className={styles.buttonsTable}>
+                                <button className={'button-primary-outline '}>Details</button>
+                                <button className={'button-primary-outline '} onClick={() => setConfirm(true)}>Cancel</button>
+                            </div>
+                        </th>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        <div className={styles.containerFunction}>
+            <select>
+                <option selected value="-">-</option>
+                <option value="allowReservations">Povolit rezervaci</option>
+                <option value="DisableReservations">Zakázat rezervaci</option>
+                <option value="Remove">Odstranit</option>
+                <option value="writeEmail">Napsat e-mail</option>
+                <option value="writeSms">Napsat sms</option>
+            </select>
+            <button className={'button-primary sm '}>Apply</button>
+        </div></div>
+    )
+}
 
 export const ReservationsPageClient = () => {
     return(
         <div className={styles.container}>
             <TopButtons/>
             <DatePicker/>
+            <TableUnder/>
         </div>
     )
 }
