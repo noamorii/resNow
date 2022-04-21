4dcb5ab6aaaf7d2da274a8541f2a37995b43b80f
Jan Pivoňka <pivonja1@fel.cvut.cz>
2022-04-21 13:13:24 +0200

    CategoriesController implemented, one method skipped on @belkapre's request

--- src/main/java/cz/cvut/fel/rsp/ReservationSystem/rest/DTO/CategoryDTO.java
+++ src/main/java/cz/cvut/fel/rsp/ReservationSystem/rest/DTO/CategoryDTO.java
@@ -1,11 +1,31 @@
 package cz.cvut.fel.rsp.ReservationSystem.rest.DTO;
 
+import cz.cvut.fel.rsp.ReservationSystem.model.reservation.Category;
+import cz.cvut.fel.rsp.ReservationSystem.model.reservation.Source;
+import cz.cvut.fel.rsp.ReservationSystem.model.reservation.events.Event;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class CategoryDTO {
+
+    private String name;
+
+    private List<Integer> sourcesIds;
+
+    private List<Integer> eventsIds;
+
+    public CategoryDTO(Category category) {
+        this.name = category.getName();
+        this.sourcesIds = category.getSources().stream()
+                .map(Source::getId)
+                .collect(Collectors.toList());
+        this.eventsIds = category.getEvents().stream()
+                .map(Event::getId)
+                .collect(Collectors.toList());
+    }
 }
