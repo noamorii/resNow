ecf741637b514f05f81b4608dd15a26021774930
najmanmat <matous@najman.cz>
2022-05-04 21:15:00 +0200

    data mapping done, with err

--- src/frontend/src/pages/client/reservations-page/ReservationsPage.client.js
+++ src/frontend/src/pages/client/reservations-page/ReservationsPage.client.js
@@ -4,6 +4,7 @@ import React, {useEffect, useState} from "react";
 import axios from "axios";
 import {baseUrl} from "../../../config/const";
 import ModalReservationDetails from "./modalWindowReservationDetails/ModalReservationDetails"
+import authHeader from "../../../services/auth-header";
 
 const TopButtons = () => {
 
@@ -12,24 +13,114 @@ const TopButtons = () => {
             <p className={styles.heading}>DAILY RESERVATION OVERVIEW</p>
             <button className={'button-primary '.concat(styles.topbutton)}>Filter</button>
             <button className={'button-primary '.concat(styles.topbutton)}>Statistics</button>
-            <button className={'button-primary '.concat(styles.topbutton)}>Export of reservations</button>
+            <button className={'button-primary '.concat(styles.topbutton)}>Export reservations</button>
             <button className={'button-primary '.concat(styles.topbutton)}>New reservation</button>
         </div>
     )
 }
 
+const Table = () => {
+    const [date, setDate] = useState(new DateObject())
 
-const DatePicker = () => {
+    const [data, setData] = useState([]);
+    const [detailsShown, setDetailsShown] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
 
-    const [value, setValue] = useState(new DateObject())
+    const onChange = (e) => {
+        //console.log(e)
+        //console.log(value.format("YYYY-MM-DD"))
+        sendGetRequest(date.format("YYYY-MM-DD"))
+        toggleShown(data[0].reservationId)
+    }
 
-    const [data1, setData] = useState()
+    useEffect(()=>{
+        sendGetRequest(date.format("YYYY-MM-DD"))
+    }, [date])
 
-    const getData = (e) => {
-        axios.get(`${baseUrl}/systems/1/`).then(result => setData(result.data.name))
-        setValue(e)
-        console.log(data1)
-        //console.log(value)
+    const sendGetRequest = async (date) => {
+        let system = 2;
+        try {
+            const resp = await axios.get(
+                `${baseUrl}/systems/${system}/reservations/`,
+                {params: {fromDate: date, toDate: date},
+                    headers: authHeader()});
+            let newData = []
+            resp.data.forEach(reservation => {
+                const dateFromRequest = concatReservationDetails(reservation)
+            })
+            setData(resp.data)
+            console.log(resp.data);
+        } catch (err) {
+            // Handle Error Here
+            console.error(err);
+        }
+    };
+
+    const concatReservationDetails = async (reservation) => {
+        try {
+            //----------------------------------------
+            const respUser = await axios.get(
+                `${baseUrl}/users/${reservation.username}`,
+                {headers: authHeader()},
+            )
+            reservation["userEmail"] = respUser.data.email;
+            reservation["userFirstName"] = respUser.data.firstName;
+            reservation["userLastName"] = respUser.data.lastName;
+
+            const respSlot = await axios.get(
+                `${baseUrl}/slots/${reservation.reservationSlotId}`,
+                {headers: authHeader()}
+            )
+            reservation["date"] = respSlot.data.date;
+            reservation["pricing"] = respSlot.data.price;
+
+            const respEvent = await axios.get(
+                `${baseUrl}/events/${respSlot.data.eventId}`,
+                {headers: authHeader()}
+            )
+            reservation["fromTime"] = respEvent.data.fromTime;
+            reservation["toTime"] = respEvent.data.toTime;
+
+            const respCategory = await axios.get(
+                `${baseUrl}/categories/${respEvent.data.categoryId}`,
+                {headers: authHeader()}
+            )
+
+            const sources = []
+            for (const sourceId of respCategory.data.sourcesIds) {
+                const respSource = await axios.get(
+                    `${baseUrl}/sources/${sourceId}`,
+                    {headers: authHeader()}
+                )
+                sources.push(respSource.data.name)
+            }
+            reservation["sources"] = sources;
+
+            reservation["capacity"] = 1;
+            if (reservation.cancelled === false){
+                reservation["state"] = "Active"
+            }
+            if (reservation.cancelled === true){
+                reservation["state"] = "Cancelled"
+            }
+            return reservation
+        } catch (err) {
+            // Handle Error Here
+            console.error(err);
+        }
+    };
+
+
+    const toggleShown = id => {
+        const shownState =  detailsShown.slice();
+        const index = shownState.indexOf(id)
+        if (index >= 0){
+            shownState.splice(index, 1);
+            setDetailsShown(shownState)
+        } else {
+            shownState.push(id);
+            setDetailsShown(shownState);
+        }
     }
 
     return (
@@ -38,95 +129,25 @@ const DatePicker = () => {
                 numberOfMonths={4}
                 disableMonthPicker
                 disableYearPicker
-                value={value}
-                onChange={e => getData(e)}
+                value={date}
+                onChange={setDate}
             />
             <div className={styles.undercalendar}>
                 <div className={styles.datedisplay}>
                     <p className={styles.datedisplay}>
-                        {value?.format("dddd MMMM D, YYYY")}
+                        {date?.format("dddd MMMM D, YYYY")}
                     </p>
                 </div>
                 <div className={'calendar-description '.concat(styles.caldescr)}>
 
                     <p className={' '.concat(styles.pdescr)}>
-                        RESERVATIONS: {}1
+                        RESERVATIONS: {data.length}
                     </p>
                     <p className={' '.concat(styles.pdescr)}>
                         RESERVED CAPACITY: {}1
                     </p>
-                    <p className={' '.concat(styles.pdescr)}>
-                        PRICE OF RESERVATIONS: {}250,-
-                    </p>
                 </div>
             </div>
-        </div>
-    )
-}
-
-const TableUnder = () => {
-    const [data, setData] = useState([
-        {
-            date: '15-04-2022',
-            start: '14:00',
-            end: '16:00',
-            code: 'EL4-MF1-1EM',
-            price: '250',
-            capacity: 1,
-            customer: 'Tonny Stark',
-            state: 'NEW',
-            cancelDate: '-',
-            phone: '123456789',
-            place: 'Tennis court',
-            service: 'Coach',
-            employee: 'Pepper',
-            created: '22-03-2022'
-        },
-        {
-            date: '15-04-2022',
-            start: '14:00',
-            end: '16:00',
-            code: 'EL4-MF1-2OP',
-            price: '250',
-            capacity: 1,
-            customer: 'Tonny Stark',
-            state: 'NEW',
-            cancelDate: '-',
-            phone: '123456789',
-            place: 'Tennis court',
-            service: 'Coach',
-            employee: 'Pepper',
-            created: '22-03-2022'
-        }
-    ]);
-    const [detailsShown, setDeatilsShown] = useState([]);
-    const [openModal, setOpenModal] = useState(false);
-
-    useEffect(()=>{
-        getData();
-    }, [])
-
-    async function getData() {
-        const result = await fetch('')
-        const getResults = await result.json();
-        setData(getResults);
-        console.log(getResults)
-    }
-
-    const toggleShown = code => {
-        const shownState =  detailsShown.slice();
-        const index = shownState.indexOf(code)
-        if (index >= 0){
-            shownState.splice(index, 1);
-            setDeatilsShown(shownState)
-        } else {
-            shownState.push(code);
-            setDeatilsShown(shownState);
-        }
-    }
-
-    return (
-        <div>
             {openModal && <ModalReservationDetails closeModal={setOpenModal}/>}
             <div className={styles.tableContainer}>
                 <table>
@@ -156,30 +177,30 @@ const TableUnder = () => {
                             <p>STATE</p>
                         </td>
                         <td className={styles.collWide}>
-                            <p>DATE OF CANCELLATION</p>
+                            <p>DATE</p>
                         </td>
                         <td className={styles.collMid}> </td>
                     </tr>
                     </thead>
                     <tbody>
                         {data.map(reservation => (
-                            <React.Fragment key={reservation.code}>
+                            <React.Fragment key={reservation.reservationId}>
                             <tr className={styles.headRow}>
                                 <td className={styles.collCheckbox}>
                                     <input className={'checkbox'} type="checkbox" />
-                                    <button onClick={() => toggleShown(reservation.code)}>
+                                    <button onClick={() => toggleShown(reservation.reservationId)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="19" fill="none" viewBox="0 0 16 19">
                                             <path fill="#000"  d="M.685 6.14 2.05 4.55l5.581 6.524 5.593-6.51 1.362 1.593-6.958 8.1L.685 6.14Z"/>
                                         </svg>
                                     </button>
                                 </td>
-                                <td>{reservation.start}-{reservation.end}</td>
-                                <td>{reservation.code}</td>
-                                <td>{reservation.price}</td>
+                                <td>{reservation.fromTime}-{reservation.toTime}</td>
+                                <td>{reservation.reservationId}</td>
+                                <td><b>{reservation.pricing} $</b></td>
                                 <td>{reservation.capacity}</td>
-                                <td>{reservation.customer}</td>
+                                <td>{reservation.userFirstName} {reservation.userLastName}</td>
                                 <td>{reservation.state}</td>
-                                <td>{reservation.cancelDate}</td>
+                                <td>{reservation.date}</td>
                                 <td>
                                     <div className={styles.buttonsTable}>
                                         <div className={styles.detailsButton}>
@@ -189,25 +210,19 @@ const TableUnder = () => {
                                     </div>
                                 </td>
                             </tr>
-                                {detailsShown.includes(reservation.code) && (
+                                {detailsShown.includes(reservation.reservationId) && (
                                     <tr>
                                         <td colSpan="2" className={styles.toggleInfo}>
                                             PHONE NUMBER: <br/>{reservation.phone}</td>
-                                        <td className={styles.toggleInfo}>PLACE: <br/>{reservation.place}</td>
-                                        <td colSpan="2" className={styles.toggleInfo}>SERVICE: <br/>{reservation.service}</td>
-                                        <td className={styles.toggleInfo}>EMPLOYEE: <br/>{reservation.employee}</td>
-                                        <td colSpan="2" className={styles.toggleInfo}>DATE OF RESERVATION: <br/>{reservation.created}</td>
+                                        <td colSpan="3" className={styles.toggleInfo}>SOURCES: <br/>{reservation.sources}</td>
+                                        <td colSpan="2" className={styles.toggleInfo}>E-MAIL: <br/>{reservation.userEmail}</td>
+                                        <td colSpan="1" className={styles.toggleInfo}>DATE OF RESERVATION: <br/>{reservation.created}</td>
                                         <td> </td>
                                     </tr>
                                 )}
                             </React.Fragment>
                         ))}
                         <div className={styles.containerFunction}>
-                            <select>
-                                <option selected value="-">-</option>
-                                <option value="allowReservations">test</option>
-                            </select>
-                            <button className={'button-primary sm '}>Apply</button>
                         </div>
                     </tbody>
                 </table>
@@ -221,8 +236,7 @@ export const ReservationsPageClient = () => {
     return(
         <div className={styles.container}>
             <TopButtons/>
-            <DatePicker/>
-            <TableUnder/>
+            <Table/>
         </div>
     )
 }
